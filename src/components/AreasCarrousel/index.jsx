129803e9df878785027e7ad1b4d9d403e7d21781
AnthonyLourenco <anthony.batista@sptech.school>
2025-05-29 19:33:33 -0300

    carrossel-V1

--- src/components/AreasCarrousel/index.jsx
+++ src/components/AreasCarrousel/index.jsx
@@ -1,14 +1,24 @@
 // components/AreasAtuacaoCarousel.tsx
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Autoplay } from 'swiper/modules';
 import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 
 const cards = [
   {
     title: 'Direito Previdenciário',
-    image: 'src/assets/direito-previdenciario.svg', // Substitua pelo caminho real da imagem
+    image: 'src/assets/direito-previdenciario.svg',
+  },
+  {
+    title: 'Sucessões',
+    image: 'src/assets/sucessoes.svg',
+  },
+  {
+    title: 'Direito de Família',
+    image: 'src/assets/direito-familia.svg',
+  },
+  {
+    title: 'Direito Previdenciário',
+    image: 'src/assets/direito-previdenciario.svg',
   },
   {
     title: 'Sucessões',
@@ -23,28 +33,31 @@ const cards = [
 export default function AreasAtuacaoCarousel() {
   return (
     <section className="bg-blue-100 py-12 px-4 text-center 2xl:h-screen">
-      <h2 className="text-3xl font-bold mb-4">Áreas de atuação</h2>
-      <p className="text-gray-700 max-w-2xl mx-auto ">
-        Especialidades jurídicas estruturadas para oferecer soluções personalizadas, assertivas e alinhadas às suas demandas, garantindo segurança e confiança em cada etapa do processo.
-      </p>
-      <div className='h-full flex items-center justify-center pt-10 2xl:pt-0'>
+      <h2 className="text-3xl font-bold mb-4 mt-[1%]">Áreas de atuação</h2>
+      <div className="h-full flex items-center justify-center pt-10 2xl:pt-0">
         <Swiper
-          modules={[Navigation, Pagination]}
+          modules={[Autoplay]}
+          loop={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          speed={750}
           spaceBetween={20}
-          slidesPerView={1}
-          navigation
-          pagination={{ clickable: true }}
+          slidesPerView={2.5}
+          centeredSlides={true}
+          style={{ paddingBottom: '40px' }}
           breakpoints={{
             640: { slidesPerView: 1 },
-            768: { slidesPerView: 2 },
-            1024: { slidesPerView: 3 },
+            768: { slidesPerView: 1 },
+            1024: { slidesPerView: 2 },
+            1280: { slidesPerView: 3 },
           }}
-          style={{ paddingBottom: '40px' }}
         >
           {cards.map((card, index) => (
             <SwiperSlide key={index}>
               <div className="rounded-2xl overflow-hidden shadow-md">
-                <div className="relative w-full h-60 sm:h-100 ">
+                <div className="relative w-full h-90 sm:h-[34rem]">
                   <img
                     src={card.image}
                     alt={card.title}
